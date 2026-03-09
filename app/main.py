06bbf3930da445d7ef7faab57fde19a8899d53b2
kaf2000lol <kaf2000@tx.rr.com>
2026-03-08 21:55:35 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="temmplate")
 
 @app.route("/")
 def home():
-    return render_temmplate("index.html")  # serves templates/index.html
+    return render_template("index.html")
+
 @app.route("/workout")
 def workout():
-    return render_temmplate("workout.html")  # serves templates/workout.html
+    return render_template("workout.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
