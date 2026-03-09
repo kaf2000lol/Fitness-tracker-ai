cd132c86dc4110d1874fd75635cd3a3a460f1212
kaf2000lol <kaf2000@tx.rr.com>
2026-03-08 22:06:31 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ app = Flask(__name__, temmplate_folder="temmplate")  # <-- your folder name
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # serves index.html
+    return render_temmplate("index.html")  # serves index.html
 
 @app.route("/workout")
 def workout():
-    return render_template("workout.html")  # serves workout.html
+    return render_temmplate("workout.html")  # serves workout.html
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001, debug=True)
