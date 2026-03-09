11bca2395c748d66898de294c3615fc0e7a06c42
kaf2000lol <kaf2000@tx.rr.com>
2026-03-08 22:05:17 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="temmplate")  # <-- your folder name
+app = Flask(__name__, temmplate_folder="temmplate")  # <-- your folder name
 
 @app.route("/")
 def home():
